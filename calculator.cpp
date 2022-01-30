97fb59cf452f7588d0781e5872a20b6fd8ae6c30
Maxcode123 <nikiforos@live.co.uk>
2022-01-30 17:45:58 +0200

    initial commit, calculator, std_lib_facilities

---
+++ calculator.cpp
@@ -0,0 +1,182 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+#include "std_lib_facilities.h"
+
+double expression();
+
+class Token
+{
+public:
+    char kind;
+    double value;
+    Token(char k) :kind{k}, value{0.0} {}
+    Token(char k, double v) :kind{k}, value(v) {}
+};
+
+class Token_stream
+{
+public:
+    Token get();
+    void putback(Token t);
+private:
+    bool full {false};
+    Token buffer {0};
+};
+
+const char number = '8';
+const char quit = 'q';
+const char print = ';';
+
+Token Token_stream::get()
+{
+    if (full) {
+        full = false;
+        return buffer;
+    }
+    char ch;
+    cin >> ch;
+
+    switch (ch) {
+        case print:
+        case quit:
+        case '(':
+        case ')':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return Token(ch);
+        case '.': case '0': case '1': case '2': case '3': case '4': 
+        case '5': case '6': case '7': case '8': case '9':
+        {
+            cin.putback(ch);
+            double val;
+            cin >> val;
+            return Token(number, val);
+        }
+        default:
+            error("Bad token\n");
+    }
+}
+
+void Token_stream::putback(Token t) 
+{
+    if (full) error("putback() into a full buffer");
+    buffer = t;
+    full = true;
+}
+
+Token_stream ts;
+
+double primary()
+{
+    Token t = ts.get();
+    switch (t.kind) {
+        case '(':
+        {
+            double d = expression();
+            t = ts.get();
+            if (t.kind != ')') error("')' expected");
+            return d;
+        }
+        case number:
+        {
+            return t.value;
+        }
+        case '-':
+        {
+            return - primary();
+        }
+        case '+':
+        {
+            return primary();
+        }
+        default:
+            error("primary expected");
+    }
+}
+
+double term()
+{
+    double left = primary();
+    Token t = ts.get();
+
+    while (true) {
+        switch (t.kind) {
+            case '*':
+            {
+                left *= primary();
+                t = ts.get();
+                break;
+            }
+            case '/':
+            {
+                double d = primary();
+                if (d == 0) error("divide by zero");
+                left /= d;
+                t = ts.get();
+                break;
+            }
+            default:
+            {
+                ts.putback(t);
+                return left;
+            }
+        }
+    }
+}
+
+double expression()
+{
+    double left = term();
+    Token t = ts.get();
+
+    while (true) {
+        switch (t.kind) {
+            case '+':
+            {
+                left += term();
+                t = ts.get();
+                break;
+            }
+            case '-':
+            {
+                left -= term();
+                t = ts.get();
+                break;
+            }
+            default:
+            {
+                ts.putback(t);
+                return left;
+            }
+        }
+    }
+}
+
+const char prompt = '>';
+const char result = '=';
+
+int main()
+try 
+{
+    while (cin) {
+        cout << prompt << ' ';
+        Token t = ts.get();
+        while (t.kind == print) t = ts.get();
+        if (t.kind == quit) return 0;
+        ts.putback(t);
+        cout << result << ' ' << expression() << '\n';        
+    }
+}
+catch (exception& e) {
+    cerr << e.what() << '\n';
+    return 1;
+}
+catch (...) {
+    cerr << "exception\n";
+    return 2;
+}
+
+#pragma GCC diagnostic pop
